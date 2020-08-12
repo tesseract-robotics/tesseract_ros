2e7684904b5581fca78752e62fde5c1633fdd4fa
Levi Armstrong <levi.armstrong@gmail.com>
2020-08-12 12:51:38 -0500

    Add planning server package

---
+++ tesseract_ros/tesseract_planning_server/examples/server_raster_example.cpp
@@ -0,0 +1,26 @@
+#include <tesseract_common/macros.h>
+TESSERACT_COMMON_IGNORE_WARNINGS_PUSH
+#include <ros/ros.h>
+TESSERACT_COMMON_IGNORE_WARNINGS_POP
+
+#include <tesseract_environment/core/environment.h>
+#include <tesseract_planning_server/tesseract_planning_server.h>
+#include <tesseract_process_managers/examples/raster_example_program.h>
+
+using namespace tesseract_environment;
+using namespace tesseract_planning;
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "server_raster_example");
+  ros::NodeHandle pnh("~");
+
+  Instruction program = rasterExampleProgram();
+
+  ros::AsyncSpinner spinner(4);
+  spinner.start();
+
+  ros::waitForShutdown();
+
+  return 0;
+}
