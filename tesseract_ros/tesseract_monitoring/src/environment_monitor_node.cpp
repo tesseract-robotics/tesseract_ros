1032b6f45407bd21d38f8479f49a4696f809995e
Levi Armstrong <levi.armstrong@gmail.com>
2020-08-18 21:42:30 -0500

    Add ability to configure the environment_monitor to listen to an external environment's topic

--- tesseract_ros/tesseract_monitoring/src/environment_monitor_node.cpp
+++ tesseract_ros/tesseract_monitoring/src/environment_monitor_node.cpp
@@ -18,21 +18,21 @@ int main(int argc, char** argv)
   std::string descrete_plugin;
   std::string continuous_plugin;
   std::string joint_state_topic;
-  std::string monitored_environment_topic;
+  std::string published_environment_topic;
 
   pnh.param<std::string>("robot_description", robot_description, ROBOT_DESCRIPTION_PARAM);
   pnh.param<std::string>("descrete_plugin", descrete_plugin, "");
   pnh.param<std::string>("continuous_plugin", continuous_plugin, "");
   pnh.param<std::string>("joint_state_topic", joint_state_topic, "");
-  pnh.param<std::string>("monitored_environment_topic", monitored_environment_topic, "");
+  pnh.param<std::string>("published_environment_topic", published_environment_topic, "");
 
   tesseract_monitoring::EnvironmentMonitor monitor(robot_description, "", descrete_plugin, continuous_plugin);
 
-  if (monitored_environment_topic.empty())
+  if (published_environment_topic.empty())
     monitor.startPublishingEnvironment(tesseract_monitoring::EnvironmentMonitor::UPDATE_ENVIRONMENT);
   else
     monitor.startPublishingEnvironment(tesseract_monitoring::EnvironmentMonitor::UPDATE_ENVIRONMENT,
-                                       monitored_environment_topic);
+                                       published_environment_topic);
 
   if (joint_state_topic.empty())
     monitor.startStateMonitor();
