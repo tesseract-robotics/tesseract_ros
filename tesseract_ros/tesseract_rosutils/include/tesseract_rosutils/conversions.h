9684579b308cc066c14b1d3ffc254a0454575f34
Levi Armstrong <levi.armstrong@gmail.com>
2020-08-27 16:52:09 -0500

    Clean up tesseract_rosutils

--- tesseract_ros/tesseract_rosutils/include/tesseract_rosutils/conversions.h
+++ tesseract_ros/tesseract_rosutils/include/tesseract_rosutils/conversions.h
@@ -1,3 +1,28 @@
+/**
+ * @file conversions.h
+ * @brief Tesseract ROS Utils conversions
+ *
+ * @author Levi Armstrong
+ * @date April 15, 2018
+ * @version TODO
+ * @bug No known bugs
+ *
+ * @copyright Copyright (c) 2018, Southwest Research Institute
+ *
+ * @par License
+ * Software License Agreement (Apache License)
+ * @par
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * @par
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #ifndef TESSERACT_ROSUTILS_CONVERSIONS_H
 #define TESSERACT_ROSUTILS_CONVERSIONS_H
 
@@ -5,23 +30,13 @@
 TESSERACT_COMMON_IGNORE_WARNINGS_PUSH
 #include <Eigen/Core>
 #include <Eigen/Geometry>
-#include <ros/console.h>
 #include <sensor_msgs/JointState.h>
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/PoseArray.h>
 #include <trajectory_msgs/JointTrajectory.h>
-#include <eigen_conversions/eigen_msg.h>
-#include <iostream>
-#include <fstream>
-#include <tesseract_msgs/ProcessPlan.h>
-#include <tesseract_msgs/ProcessPlanPath.h>
 #include <tesseract_msgs/ProcessPlanSegment.h>
-#include <tesseract_msgs/ProcessPlanTransitionPair.h>
-#include <boost/algorithm/string/classification.hpp>
-#include <boost/algorithm/string.hpp>
+#include <tesseract_msgs/ProcessPlan.h>
 TESSERACT_COMMON_IGNORE_WARNINGS_POP
-
-#include <tesseract_rosutils/utils.h>
 
 namespace tesseract_rosutils
 {
@@ -30,10 +45,7 @@ namespace tesseract_rosutils
  * @param vector The STD Vector to be converted
  * @return Eigen::VectorXd
  */
-inline Eigen::VectorXd toEigen(const std::vector<double>& vector)
-{
-  return Eigen::VectorXd::Map(vector.data(), static_cast<long>(vector.size()));
-}
+Eigen::VectorXd toEigen(const std::vector<double>& vector);
 
 /**
  * @brief Converts JointState position to Eigen vector in the order provided by joint_names
@@ -41,22 +53,51 @@ inline Eigen::VectorXd toEigen(const std::vector<double>& vector)
  * @param joint_names The vector joint names used to order the output
  * @return Eigen::VectorXd in the same order as joint_names
  */
-inline Eigen::VectorXd toEigen(const sensor_msgs::JointState& joint_state, const std::vector<std::string>& joint_names)
-{
-  Eigen::VectorXd position;
-  position.resize(static_cast<long>(joint_names.size()));
-  int i = 0;
-  for (const auto& joint_name : joint_names)
-  {
-    auto it = std::find(joint_state.name.begin(), joint_state.name.end(), joint_name);
-    assert(it != joint_state.name.end());
-    size_t index = static_cast<size_t>(std::distance(joint_state.name.begin(), it));
-    position[i] = joint_state.position[index];
-    ++i;
-  }
+Eigen::VectorXd toEigen(const sensor_msgs::JointState& joint_state, const std::vector<std::string>& joint_names);
 
-  return position;
-}
+/**
+ * @brief Append a process segment to an existing joint_trajectory
+ * @param joint_trajectory Trajectory to add the process segment
+ * @param process_plan_segment Process plan segment
+ * @return
+ */
+bool toJointTrajectory(trajectory_msgs::JointTrajectory& joint_trajectory,
+                       const tesseract_msgs::ProcessPlanSegment& process_plan_segment);
+
+/**
+ * @brief Convert a Process Plan to a single Joint Trajectory for visualization
+ * This does not clear the trajectory passed in it appends.
+ * @param joint_trajectory Joint Trajectory Message
+ * @param process_plan Process Plan
+ * @return True if successful, otherwise false
+ */
+bool toJointTrajectory(trajectory_msgs::JointTrajectory& joint_trajectory,
+                              const tesseract_msgs::ProcessPlan& process_plan);
+
+/**
+ * @brief Convert a joint trajector to csv formate and write to file
+ * @param joint_trajectory Joint trajectory to be writen to file
+ * @param file_path The location to save the file
+ * @param separator The separator to use
+ * @return true if successful
+ */
+bool toCSVFile(const trajectory_msgs::JointTrajectory& joint_trajectory, const std::string& file_path, char separator = ',');
+
+/**
+ * @brief Convert CSVFile to JointTrajectory
+ * @param file_path File path to CSV
+ * @param separator The separator to use
+ * @return Joint Trajectory
+ */
+trajectory_msgs::JointTrajectory jointTrajectoryFromCSVFile(const std::string& file_path, char separator = ',');
+
+/**
+ * @brief Convert a Tesseract Trajectory to Process Plan Path
+ * @param trajectory Tesseract Trajectory
+ * @param joint_names Joint names corresponding to the tesseract trajectory
+ * @return A process plan path
+ */
+tesseract_msgs::ProcessPlanPath toProcessPlanPath(const tesseract_common::JointTrajectory& joint_trajectory);
 
 ///**
 // * @brief Convert a cartesian pose to cartesian waypoint.
@@ -134,20 +175,6 @@ inline Eigen::VectorXd toEigen(const sensor_msgs::JointState& joint_state, const
 //  return std::make_shared<tesseract_motion_planners::JointWaypoint>(joint_positions, joint_names);
 //}
 
-/**
- * @brief Convert a Tesseract Trajectory to Process Plan Path
- * @param trajectory Tesseract Trajectory
- * @param joint_names Joint names corresponding to the tesseract trajectory
- * @return A process plan path
- */
-inline tesseract_msgs::ProcessPlanPath toProcessPlanPath(const tesseract_common::JointTrajectory& joint_trajectory)
-{
-  tesseract_msgs::ProcessPlanPath path;
-  if (joint_trajectory.trajectory.size() != 0)
-    tesseract_rosutils::toMsg(path.trajectory, joint_trajectory);
-  return path;
-}
-
 ///**
 // * @brief Convert a process plan segment to a process plan segment message
 // * @param segment_results The process segment plan
@@ -163,183 +190,6 @@ inline tesseract_msgs::ProcessPlanPath toProcessPlanPath(const tesseract_common:
 //  process_segment.departure = toProcessPlanPath(process_plan_segment.departure);
 //  return process_segment;
 //}
-
-/**
- * @brief Append a process segment to an existing joint_trajectory
- * @param joint_trajectory Trajectory to add the process segment
- * @param process_plan_segment Process plan segment
- * @return
- */
-inline bool toJointTrajectory(trajectory_msgs::JointTrajectory& joint_trajectory,
-                              const tesseract_msgs::ProcessPlanSegment& process_plan_segment)
-{
-  double t = 0;
-  if (!joint_trajectory.points.empty())
-    t = joint_trajectory.points.back().time_from_start.toSec();
-
-  for (const auto& point : process_plan_segment.approach.trajectory.points)
-  {
-    joint_trajectory.points.push_back(point);
-    joint_trajectory.points.back().time_from_start.fromSec(t + point.time_from_start.toSec());
-  }
-  t = joint_trajectory.points.back().time_from_start.toSec();
-
-  for (const auto& point : process_plan_segment.process.trajectory.points)
-  {
-    joint_trajectory.points.push_back(point);
-    joint_trajectory.points.back().time_from_start.fromSec(t + point.time_from_start.toSec());
-  }
-  t = joint_trajectory.points.back().time_from_start.toSec();
-
-  for (const auto& point : process_plan_segment.departure.trajectory.points)
-  {
-    joint_trajectory.points.push_back(point);
-    joint_trajectory.points.back().time_from_start.fromSec(t + point.time_from_start.toSec());
-  }
-
-  return true;
-}
-
-/**
- * @brief Convert a Process Plan to a single Joint Trajectory for visualization
- * This does not clear the trajectory passed in it appends.
- * @param joint_trajectory Joint Trajectory Message
- * @param process_plan Process Plan
- * @return True if successful, otherwise false
- */
-inline bool toJointTrajectory(trajectory_msgs::JointTrajectory& joint_trajectory,
-                              const tesseract_msgs::ProcessPlan& process_plan)
-{
-  double t = 0;
-  if (!joint_trajectory.points.empty())
-    t = joint_trajectory.points.back().time_from_start.toSec();
-
-  for (const auto& point : process_plan.from_start.trajectory.points)
-  {
-    joint_trajectory.points.push_back(point);
-    joint_trajectory.points.back().time_from_start.fromSec(t + point.time_from_start.toSec());
-  }
-  t = joint_trajectory.points.back().time_from_start.toSec();
-
-  if (!process_plan.from_start.trajectory.points.empty())
-  {
-    joint_trajectory.joint_names = process_plan.segments[0].process.trajectory.joint_names;
-    joint_trajectory.header = process_plan.segments[0].process.trajectory.header;
-  }
-
-  // Append process segments and transitions
-  for (size_t i = 0; i < process_plan.segments.size(); ++i)
-  {
-    toJointTrajectory(joint_trajectory, process_plan.segments[i]);
-    t = joint_trajectory.points.back().time_from_start.toSec();
-
-    if (i < (process_plan.segments.size() - 1))
-    {
-      for (const auto& point : process_plan.transitions[i].from_end.trajectory.points)
-      {
-        joint_trajectory.points.push_back(point);
-        joint_trajectory.points.back().time_from_start.fromSec(t + point.time_from_start.toSec());
-      }
-      t = joint_trajectory.points.back().time_from_start.toSec();
-    }
-  }
-
-  // Append path to home
-  for (const auto& point : process_plan.to_end.trajectory.points)
-  {
-    joint_trajectory.points.push_back(point);
-    joint_trajectory.points.back().time_from_start.fromSec(t + point.time_from_start.toSec());
-  }
-
-  return true;
-}
-
-/**
- * @brief Convert a joint trajector to csv formate and write to file
- * @param joint_trajectory Joint trajectory to be writen to file
- * @param file_path The location to save the file
- * @return true if successful
- */
-inline bool toCSVFile(const trajectory_msgs::JointTrajectory& joint_trajectory,
-                      const std::string& file_path,
-                      char separator = ',')
-{
-  std::ofstream myfile;
-  myfile.open(file_path);
-
-  // Write Joint names as header
-  std::copy(joint_trajectory.joint_names.begin(),
-            joint_trajectory.joint_names.end(),
-            std::ostream_iterator<std::string>(myfile, &separator));
-  myfile << ",\n";
-  for (const auto& point : joint_trajectory.points)
-  {
-    std::copy(point.positions.begin(), point.positions.end(), std::ostream_iterator<double>(myfile, &separator));
-    myfile << "," + std::to_string(point.time_from_start.toSec()) + ",\n";
-  }
-  myfile.close();
-  return true;
-}
-
-inline trajectory_msgs::JointTrajectory jointTrajectoryFromCSVFile(const std::string& file_path, char separator = ',')
-{
-  trajectory_msgs::JointTrajectory joint_trajectory;
-  std::ifstream csv_file(file_path);
-
-  // Make sure the file is open
-  if (!csv_file.is_open())
-    throw std::runtime_error("Could not open csv file");
-
-  // Helper vars
-  std::string line, column_name;
-  int num_joints = 0;
-
-  // Read the column names
-  if (csv_file.good())
-  {
-    // Extract the first line in the file
-    std::getline(csv_file, line);
-
-    // Create a stringstream from line
-    std::stringstream ss(line);
-
-    // Extract joint name
-    std::vector<std::string> column_names;
-    while (std::getline(ss, column_name, separator))
-      column_names.push_back(column_name);
-
-    for (std::size_t i = 0; i < column_names.size() - 1; ++i)
-      joint_trajectory.joint_names.push_back(column_names[i]);
-
-    num_joints = column_names.size() - 1;
-  }
-
-  // Read data, line by line
-  while (std::getline(csv_file, line))
-  {
-    std::vector<std::string> tokens;
-    boost::split(tokens, line, boost::is_any_of(std::string(&separator)), boost::token_compress_on);
-    if (!tesseract_common::isNumeric(tokens))
-      throw std::runtime_error("jointTrajectoryFromCSVFile: Invalid format");
-
-    trajectory_msgs::JointTrajectoryPoint point;
-    double tfs = 0;
-    tesseract_common::toNumeric<double>(tokens.back(), tfs);
-    point.time_from_start.fromSec(tfs);
-    for (std::size_t i = 0; i < num_joints; ++i)
-    {
-      double val = 0;
-      tesseract_common::toNumeric<double>(tokens[i], val);
-      point.positions.push_back(val);
-    }
-    joint_trajectory.points.push_back(point);
-  }
-
-  // Close file
-  csv_file.close();
-
-  return joint_trajectory;
-}
 
 }  // namespace tesseract_rosutils
 #endif  // TESSERACT_ROSUTILS_CONVERSIONS_H
---
+++ tesseract_ros/tesseract_rosutils/src/plotting.cpp
@@ -0,0 +1,392 @@
+/**
+ * @file plotting.cpp
+ * @brief Tesseract ROS plotting functions.
+ *
+ * @author Levi Armstrong
+ * @date April 15, 2018
+ * @version TODO
+ * @bug No known bugs
+ *
+ * @copyright Copyright (c) 2013, Southwest Research Institute
+ *
+ * @par License
+ * Software License Agreement (Apache License)
+ * @par
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * @par
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <tesseract_common/macros.h>
+TESSERACT_COMMON_IGNORE_WARNINGS_PUSH
+#include <ros/console.h>
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <ros/publisher.h>
+#include <tesseract_msgs/Trajectory.h>
+#include <tesseract_msgs/TesseractState.h>
+#include <Eigen/Geometry>
+#include <ros/ros.h>
+TESSERACT_COMMON_IGNORE_WARNINGS_POP
+
+#include <tesseract_visualization/visualization.h>
+#include <tesseract_rosutils/utils.h>
+#include <tesseract_environment/core/environment.h>
+#include <tesseract_command_language/core/instruction.h>
+
+namespace tesseract_rosutils
+{
+
+ROSPlotting::ROSPlotting(std::string root_link = "world", std::string topic_namespace = "tesseract")
+  : root_link_(root_link), topic_namespace_(topic_namespace)
+{
+  ros::NodeHandle nh;
+
+  trajectory_pub_ =
+      nh.advertise<tesseract_msgs::Trajectory>(topic_namespace + "/display_tesseract_trajectory", 1, true);
+  collisions_pub_ = nh.advertise<visualization_msgs::MarkerArray>(topic_namespace + "/display_collisions", 1, true);
+  arrows_pub_ = nh.advertise<visualization_msgs::MarkerArray>(topic_namespace + "/display_arrows", 1, true);
+  axes_pub_ = nh.advertise<visualization_msgs::MarkerArray>(topic_namespace + "/display_axes", 1, true);
+}
+
+void ROSPlotting::plotTrajectory(const tesseract_msgs::Trajectory& traj) { trajectory_pub_.publish(traj); }
+
+void ROSPlotting::plotTrajectory(const std::vector<std::string>& joint_names,
+                    const Eigen::Ref<const tesseract_common::TrajArray>& traj) override
+{
+  tesseract_msgs::Trajectory msg;
+  tesseract_common::JointTrajectory trajectory;
+  trajectory.joint_names = joint_names;
+  trajectory.trajectory = traj;
+
+  // Set the joint trajectory message
+  toMsg(msg.joint_trajectory, trajectory);
+
+  plotTrajectory(msg);
+}
+
+bool ROSPlotting::init(tesseract::Tesseract::ConstPtr /*thor*/) override
+{
+  // TODO
+}
+
+void ROSPlotting::plotTrajectory(const tesseract_common::JointTrajectory& traj) override
+{
+  tesseract_msgs::Trajectory msg;
+
+  // Set the joint trajectory message
+  toMsg(msg.joint_trajectory, traj);
+
+  plotTrajectory(msg);
+}
+
+void ROSPlotting::plotTrajectory(const tesseract_planning::Instruction& /*instruction*/) override
+{
+  // TODO
+}
+
+void ROSPlotting::plotTrajectory(const std::vector<std::string>& joint_names,
+                    const Eigen::Ref<const tesseract_common::TrajArray>& traj,
+                    const tesseract_environment::Environment::ConstPtr& env)
+{
+  tesseract_msgs::Trajectory msg;
+
+  // Set tesseract state information
+  toMsg(msg.tesseract_state, *env);
+
+  // Set the joint trajectory message
+  toMsg(msg.joint_trajectory, *(env->getCurrentState()), joint_names, traj);
+
+  plotTrajectory(msg);
+}
+
+void ROSPlotting::plotToolPath(const tesseract_planning::Instruction& /*instruction*/) override
+{
+  // TODO
+}
+
+
+
+visualization_msgs::MarkerArray
+getContactResultsMarkerArrayMsg(int& id_counter,
+                                const std::string& frame_id,
+                                const std::string& ns,
+                                const ros::Time& time_stamp,
+                                const std::vector<std::string>& link_names,
+                                const tesseract_collision::ContactResultVector& dist_results,
+                                const Eigen::Ref<const Eigen::VectorXd>& safety_distances)
+{
+  visualization_msgs::MarkerArray msg;
+  for (unsigned i = 0; i < dist_results.size(); ++i)
+  {
+    const tesseract_collision::ContactResult& dist = dist_results[i];
+    const double& safety_distance = safety_distances[i];
+
+    Eigen::Vector4d rgba;
+    if (dist.distance < 0)
+    {
+      rgba << 1.0, 0.0, 0.0, 1.0;
+    }
+    else if (dist.distance < safety_distance)
+    {
+      rgba << 1.0, 1.0, 0.0, 1.0;
+    }
+    else
+    {
+      rgba << 0.0, 1.0, 0.0, 1.0;
+    }
+
+    if (dist.cc_type[0] == tesseract_collision::ContinuousCollisionType::CCType_Between)
+    {
+      Eigen::Vector4d cc_rgba;
+      cc_rgba << 0.0, 0.0, 1.0, 1.0;
+      auto marker = getMarkerArrowMsg(id_counter,
+                                      frame_id,
+                                      ns,
+                                      time_stamp,
+                                      dist.transform[0] * dist.nearest_points_local[0],
+                                      dist.cc_transform[0] * dist.nearest_points_local[0],
+                                      cc_rgba,
+                                      0.01);
+      msg.markers.push_back(marker);
+    }
+
+    if (dist.cc_type[1] == tesseract_collision::ContinuousCollisionType::CCType_Between)
+    {
+      Eigen::Vector4d cc_rgba;
+      cc_rgba << 0.0, 0.0, 0.5, 1.0;
+      auto marker = getMarkerArrowMsg(id_counter,
+                                      frame_id,
+                                      ns,
+                                      time_stamp,
+                                      dist.transform[1] * dist.nearest_points_local[1],
+                                      dist.cc_transform[1] * dist.nearest_points_local[1],
+                                      cc_rgba,
+                                      0.01);
+      msg.markers.push_back(marker);
+    }
+
+    auto it0 = std::find(link_names.begin(), link_names.end(), dist.link_names[0]);
+    auto it1 = std::find(link_names.begin(), link_names.end(), dist.link_names[1]);
+
+    if (it0 != link_names.end() && it1 != link_names.end())
+    {
+      auto marker0 = getMarkerArrowMsg(
+          id_counter, frame_id, ns, time_stamp, dist.nearest_points[0], dist.nearest_points[1], rgba, 0.01);
+      msg.markers.push_back(marker0);
+
+      auto marker1 = getMarkerArrowMsg(
+          id_counter, frame_id, ns, time_stamp, dist.nearest_points[1], dist.nearest_points[0], rgba, 0.01);
+      msg.markers.push_back(marker1);
+    }
+    else if (it0 != link_names.end())
+    {
+      auto marker = getMarkerArrowMsg(
+          id_counter, frame_id, ns, time_stamp, dist.nearest_points[1], dist.nearest_points[0], rgba, 0.01);
+      msg.markers.push_back(marker);
+    }
+    else
+    {
+      auto marker = getMarkerArrowMsg(
+          id_counter, frame_id, ns, time_stamp, dist.nearest_points[0], dist.nearest_points[1], rgba, 0.01);
+      msg.markers.push_back(marker);
+    }
+  }
+
+  return msg;
+}
+
+void ROSPlotting::plotContactResults(const std::vector<std::string>& link_names,
+                        const tesseract_collision::ContactResultVector& dist_results,
+                        const Eigen::Ref<const Eigen::VectorXd>& safety_distances) override
+{
+  visualization_msgs::MarkerArray msg = getContactResultsMarkerArrayMsg(
+      marker_counter_, root_link_, topic_namespace_, ros::Time::now(), link_names, dist_results, safety_distances);
+  if (!dist_results.empty())
+  {
+    collisions_pub_.publish(msg);
+  }
+}
+
+void ROSPlotting::plotArrow(const Eigen::Ref<const Eigen::Vector3d>& pt1,
+               const Eigen::Ref<const Eigen::Vector3d>& pt2,
+               const Eigen::Ref<const Eigen::Vector4d>& rgba,
+               double scale) override
+{
+  visualization_msgs::MarkerArray msg;
+  auto marker =
+      getMarkerArrowMsg(marker_counter_, root_link_, topic_namespace_, ros::Time::now(), pt1, pt2, rgba, scale);
+  msg.markers.push_back(marker);
+  arrows_pub_.publish(msg);
+}
+
+void ROSPlotting::plotAxis(const Eigen::Isometry3d& axis, double scale) override
+{
+  visualization_msgs::MarkerArray msg;
+  Eigen::Vector3d x_axis = axis.matrix().block<3, 1>(0, 0);
+  Eigen::Vector3d y_axis = axis.matrix().block<3, 1>(0, 1);
+  Eigen::Vector3d z_axis = axis.matrix().block<3, 1>(0, 2);
+  Eigen::Vector3d position = axis.matrix().block<3, 1>(0, 3);
+
+  auto marker = getMarkerCylinderMsg(marker_counter_,
+                                     root_link_,
+                                     topic_namespace_,
+                                     ros::Time::now(),
+                                     position,
+                                     position + x_axis,
+                                     Eigen::Vector4d(1, 0, 0, 1),
+                                     scale);
+  msg.markers.push_back(marker);
+
+  marker = getMarkerCylinderMsg(marker_counter_,
+                                root_link_,
+                                topic_namespace_,
+                                ros::Time::now(),
+                                position,
+                                position + y_axis,
+                                Eigen::Vector4d(0, 1, 0, 1),
+                                scale);
+  msg.markers.push_back(marker);
+
+  marker = getMarkerCylinderMsg(marker_counter_,
+                                root_link_,
+                                topic_namespace_,
+                                ros::Time::now(),
+                                position,
+                                position + z_axis,
+                                Eigen::Vector4d(0, 0, 1, 1),
+                                scale);
+  msg.markers.push_back(marker);
+
+  axes_pub_.publish(msg);
+}
+
+void ROSPlotting::clear() override
+{
+  // Remove old arrows
+  marker_counter_ = 0;
+  visualization_msgs::MarkerArray msg;
+  visualization_msgs::Marker marker;
+  marker.header.frame_id = root_link_;
+  marker.header.stamp = ros::Time();
+  marker.ns = topic_namespace_;
+  marker.id = 0;
+  marker.type = visualization_msgs::Marker::ARROW;
+  marker.action = visualization_msgs::Marker::DELETEALL;
+  msg.markers.push_back(marker);
+  collisions_pub_.publish(msg);
+  arrows_pub_.publish(msg);
+  axes_pub_.publish(msg);
+}
+
+void ROSPlotting::waitForInput() override
+{
+  ROS_ERROR("Hit enter key to step optimization!");
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+visualization_msgs::Marker getMarkerArrowMsg(int& id_counter,
+                                                    const std::string& frame_id,
+                                                    const std::string& ns,
+                                                    const ros::Time& time_stamp,
+                                                    const Eigen::Ref<const Eigen::Vector3d>& pt1,
+                                                    const Eigen::Ref<const Eigen::Vector3d>& pt2,
+                                                    const Eigen::Ref<const Eigen::Vector4d>& rgba,
+                                                    const double scale)
+{
+  visualization_msgs::Marker marker;
+  marker.header.frame_id = frame_id;
+  marker.header.stamp = time_stamp;
+  marker.ns = ns;
+  marker.id = ++id_counter;
+  marker.type = visualization_msgs::Marker::ARROW;
+  marker.action = visualization_msgs::Marker::ADD;
+
+  Eigen::Vector3d x, y, z;
+  x = (pt2 - pt1).normalized();
+  marker.pose.position.x = pt1(0);
+  marker.pose.position.y = pt1(1);
+  marker.pose.position.z = pt1(2);
+
+  y = x.unitOrthogonal();
+  z = (x.cross(y)).normalized();
+  Eigen::Matrix3d rot;
+  rot.col(0) = x;
+  rot.col(1) = y;
+  rot.col(2) = z;
+  Eigen::Quaterniond q(rot);
+  q.normalize();
+  marker.pose.orientation.x = q.x();
+  marker.pose.orientation.y = q.y();
+  marker.pose.orientation.z = q.z();
+  marker.pose.orientation.w = q.w();
+
+  marker.scale.x = std::abs((pt2 - pt1).norm());
+  marker.scale.y = scale;
+  marker.scale.z = scale;
+
+  marker.color.r = static_cast<float>(rgba(0));
+  marker.color.g = static_cast<float>(rgba(1));
+  marker.color.b = static_cast<float>(rgba(2));
+  marker.color.a = static_cast<float>(rgba(3));
+
+  return marker;
+}
+
+visualization_msgs::Marker getMarkerCylinderMsg(int& id_counter,
+                                                       const std::string& frame_id,
+                                                       const std::string& ns,
+                                                       const ros::Time& time_stamp,
+                                                       const Eigen::Ref<const Eigen::Vector3d>& pt1,
+                                                       const Eigen::Ref<const Eigen::Vector3d>& pt2,
+                                                       const Eigen::Ref<const Eigen::Vector4d>& rgba,
+                                                       const double scale)
+{
+  visualization_msgs::Marker marker;
+  marker.header.frame_id = frame_id;
+  marker.header.stamp = time_stamp;
+  marker.ns = ns;
+  marker.id = ++id_counter;
+  marker.type = visualization_msgs::Marker::CYLINDER;
+  marker.action = visualization_msgs::Marker::ADD;
+
+  double length = scale * std::abs((pt2 - pt1).norm());
+  Eigen::Vector3d x, y, z, center;
+  z = (pt2 - pt1).normalized();
+  center = pt1 + (length / 2.0) * z;
+  marker.pose.position.x = center(0);
+  marker.pose.position.y = center(1);
+  marker.pose.position.z = center(2);
+
+  y = z.unitOrthogonal();
+  x = (y.cross(z)).normalized();
+  Eigen::Matrix3d rot;
+  rot.col(0) = x;
+  rot.col(1) = y;
+  rot.col(2) = z;
+  Eigen::Quaterniond q(rot);
+  q.normalize();
+  marker.pose.orientation.x = q.x();
+  marker.pose.orientation.y = q.y();
+  marker.pose.orientation.z = q.z();
+  marker.pose.orientation.w = q.w();
+
+  marker.scale.x = length / 20.0;
+  marker.scale.y = length / 20.0;
+  marker.scale.z = length;
+
+  marker.color.r = static_cast<float>(rgba(0));
+  marker.color.g = static_cast<float>(rgba(1));
+  marker.color.b = static_cast<float>(rgba(2));
+  marker.color.a = static_cast<float>(rgba(3));
+
+  return marker;
+}
+
+}  // namespace tesseract_rosutils
