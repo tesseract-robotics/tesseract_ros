ad06c70bdb7a464d827bce7ff738598a9ec642b9
Michael Ripperger <michael.ripperger@swri.org>
2020-12-17 15:18:49 -0600

    Moved tesseract dependencies from DEPENDS to CFG_EXTRAS

---
+++ tesseract_ros/tesseract_plugins/cmake/tesseract_plugins-extras.cmake
@@ -0,0 +1,2 @@
+find_package(tesseract REQUIRED)
+find_package(tesseract_common REQUIRED)
---
+++ tesseract_ros/tesseract_rosutils/cmake/tesseract_rosutils-extras.cmake
@@ -0,0 +1,7 @@
+find_package(tesseract_scene_graph REQUIRED)
+find_package(tesseract_geometry REQUIRED) # This should not be required, must be doing something wrong when creating targets
+find_package(tesseract_visualization REQUIRED)
+find_package(tesseract_collision REQUIRED)
+find_package(tesseract_common REQUIRED)
+find_package(tesseract_motion_planners REQUIRED)
+find_package(tesseract_process_planners REQUIRED)
