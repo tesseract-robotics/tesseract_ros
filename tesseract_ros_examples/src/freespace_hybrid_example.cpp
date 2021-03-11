17df31469d52e3203660cf89320121d9435845e1
Levi Armstrong <levi.armstrong@swri.org>
2021-03-11 00:27:30 -0600

    Update header location for profile dictionary

--- tesseract_ros_examples/src/freespace_hybrid_example.cpp
+++ tesseract_ros_examples/src/freespace_hybrid_example.cpp
@@ -38,7 +38,7 @@ TESSERACT_COMMON_IGNORE_WARNINGS_POP
 #include <tesseract_process_managers/taskflow_generators/freespace_taskflow.h>
 #include <tesseract_planning_server/tesseract_planning_server.h>
 #include <tesseract_motion_planners/ompl/profile/ompl_default_plan_profile.h>
-#include <tesseract_motion_planners/core/profile_dictionary.h>
+#include <tesseract_command_language/profile_dictionary.h>
 #include <tesseract_motion_planners/core/utils.h>
 #include <tesseract_visualization/markers/toolpath_marker.h>
 
