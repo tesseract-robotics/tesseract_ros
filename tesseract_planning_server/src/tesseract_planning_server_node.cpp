77ce0ceb97163fcd3c15acd6f54ef3ca9dbc90b4
Levi-Armstrong <levi.armstrong@gmail.com>
2021-10-08 16:01:08 -0500

    Update tesseract_planning_server

--- tesseract_planning_server/src/tesseract_planning_server_node.cpp
+++ tesseract_planning_server/src/tesseract_planning_server_node.cpp
@@ -29,7 +29,7 @@ TESSERACT_COMMON_IGNORE_WARNINGS_PUSH
 #include <memory>
 TESSERACT_COMMON_IGNORE_WARNINGS_POP
 
-#include <tesseract_environment/core/environment.h>
+#include <tesseract_environment/environment.h>
 #include <tesseract_planning_server/tesseract_planning_server.h>
 
 using namespace tesseract_environment;
