9c1789ab98bed563ded902b0148b89d1531e97eb
Levi-Armstrong <levi.armstrong@gmail.com>
2021-11-04 13:22:11 -0500

    Update leverage new graph taskflow

--- tesseract_ros_examples/src/basic_cartesian_example.cpp
+++ tesseract_ros_examples/src/basic_cartesian_example.cpp
@@ -40,7 +40,6 @@ TESSERACT_COMMON_IGNORE_WARNINGS_POP
 #include <tesseract_rosutils/utils.h>
 #include <tesseract_command_language/command_language.h>
 #include <tesseract_command_language/utils/utils.h>
-#include <tesseract_process_managers/taskflow_generators/trajopt_taskflow.h>
 #include <tesseract_process_managers/core/process_planning_server.h>
 #include <tesseract_process_managers/core/default_process_planners.h>
 #include <tesseract_planning_server/tesseract_planning_server.h>
