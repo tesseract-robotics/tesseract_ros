3ae1462169804a8d1443f0d75339fc6de3fe95c7
Levi Armstrong <levi.armstrong@gmail.com>
2025-10-28 11:27:20 -0500

    Update to tesseract version 0.33.X

--- tesseract_rosutils/src/utils.cpp
+++ tesseract_rosutils/src/utils.cpp
@@ -1992,8 +1992,9 @@ bool fromMsg(tesseract_srdf::KinematicsInformation& kin_info, const tesseract_ms
 tesseract_common::KinematicsPluginInfo fromMsg(const tesseract_msgs::KinematicsPluginInfo& info_msg)
 {
   tesseract_common::KinematicsPluginInfo info;
-  info.search_paths.insert(info_msg.search_paths.begin(), info_msg.search_paths.end());
-  info.search_libraries.insert(info_msg.search_libraries.begin(), info_msg.search_libraries.end());
+  info.search_paths.insert(info.search_paths.end(), info_msg.search_paths.begin(), info_msg.search_paths.end());
+  info.search_libraries.insert(
+      info.search_libraries.end(), info_msg.search_libraries.begin(), info_msg.search_libraries.end());
 
   for (const auto& pair : info_msg.group_fwd_plugins)
   {
@@ -2017,8 +2018,9 @@ tesseract_common::KinematicsPluginInfo fromMsg(const tesseract_msgs::KinematicsP
 tesseract_common::ContactManagersPluginInfo fromMsg(const tesseract_msgs::ContactManagersPluginInfo& info_msg)
 {
   tesseract_common::ContactManagersPluginInfo info;
-  info.search_paths.insert(info_msg.search_paths.begin(), info_msg.search_paths.end());
-  info.search_libraries.insert(info_msg.search_libraries.begin(), info_msg.search_libraries.end());
+  info.search_paths.insert(info.search_paths.end(), info_msg.search_paths.begin(), info_msg.search_paths.end());
+  info.search_libraries.insert(
+      info.search_libraries.end(), info_msg.search_libraries.begin(), info_msg.search_libraries.end());
   info.discrete_plugin_infos.default_plugin = info_msg.discrete_plugin_container.default_plugin;
   info.discrete_plugin_infos.plugins = fromMsg(info_msg.discrete_plugin_container.plugins);
   info.continuous_plugin_infos.default_plugin = info_msg.continuous_plugin_container.default_plugin;
